61656c75a8f529cef043da9aa03cd0ea96086e2a
ambidext <ambidext@naver.com>
2022-05-15 15:34:09 +0900

    commit

---
+++ HTTP_CLIENT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace HTTP_CLIENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, "http://127.0.0.1:8080/requestDate");
+            var res = client.SendAsync(httpRequest).Result;
+            Console.WriteLine("Response :" + " - " + res.Content.ReadAsStringAsync().Result);
+
+            string str = "{\"name\":\"Test1234\"}";
+            httpRequest = new HttpRequestMessage(HttpMethod.Post, "http://127.0.0.1:8080/sendTextFile");
+            httpRequest.Content = new StringContent(str, Encoding.UTF8, "application/json");
+            res = client.SendAsync(httpRequest).Result;
+            Console.WriteLine("Response :" + " - " + res.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
---
+++ HTTP_SERVER/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+
+namespace HTTP_SERVER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://127.0.0.1:8080/");
+            listener.Start();
+            while (true)
+            {
+                var context = listener.GetContext();
+                Console.WriteLine(string.Format("Request({0}) : {1}", context.Request.HttpMethod, context.Request.Url));
+
+                DateTime dt = DateTime.Now;
+                String strRes = "";
+                if (context.Request.HttpMethod == "GET")
+                {
+                    
+                    strRes = dt.ToString();
+                }
+                else if (context.Request.HttpMethod == "POST")
+                {
+                    strRes = "(POST) " + dt.ToString();
+                    System.IO.Stream body = context.Request.InputStream;
+                    System.Text.Encoding encoding = context.Request.ContentEncoding;
+                    System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+
+                    string strReceive = reader.ReadToEnd();
+                    JObject jsonObj = new JObject();
+                    jsonObj = JObject.Parse(strReceive);
+                    string name = jsonObj["name"].ToString();
+                    Console.WriteLine("body : " + body.ToString());
+                    Console.WriteLine("strReceive : " + strReceive);
+
+                    body.Close();
+                    reader.Close();
+                }
+                byte[] data = Encoding.UTF8.GetBytes(strRes);
+                context.Response.OutputStream.Write(data, 0, data.Length);
+                context.Response.StatusCode = 200;
+                context.Response.Close();
+            }
+        }
+    }
+}
