0cf5969d6d83c8957817dc68167bbc3775b54400
ambidext <ambidext@naver.com>
2022-10-16 11:08:02 +0900

    Prac01 Test

--- Test/Program.cs
+++ Test/Program.cs
@@ -15,6 +15,7 @@ namespace JsonTest
         static void Main(string[] args)
         {
             Console.WriteLine("Branch Prac01 Test");
+            Console.WriteLine("Prac01 Test");
             if (args.Length < 2)
             {
                 Console.WriteLine("Base64Enc.exe [src file] [des file]");
